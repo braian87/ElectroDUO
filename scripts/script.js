98c2945ba8e17994b1fe33d9afa72891721210e7
braian87 <104805817+braian87@users.noreply.github.com>
2025-03-19 21:25:54 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
+// Funcionalidad del menú desplegable para dispositivos móviles
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+
+
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
   
